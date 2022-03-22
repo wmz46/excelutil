432672c371ecfcf662dd647b8213bdce592c128b
钢翼 <659240788@qq.com>
2022-03-22 14:47:49 +0800

    增加单元测试

---
+++ src/test/java/Tests.java
@@ -0,0 +1,43 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.iceolive.util.ExcelUtil;
+import com.iceolive.util.annotation.ExcelColumn;
+import com.iceolive.util.model.ImportResult;
+import lombok.Data;
+import org.junit.Test;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Tests {
+    @Data
+    public static class TestModel {
+        @NotNull //验证注解
+        @ExcelColumn("年龄")//注解excel的列标题
+        private Integer age;
+        @NotBlank//验证注解
+        @ExcelColumn("姓名")//注解excel的列标题
+        private String name;
+        @ExcelColumn(trueString = "是",falseString = "否") //支持自定义布尔值
+        private Boolean agree;
+        @ExcelColumn//支持日期类型
+        @JsonFormat(pattern = "yyyy-MM-dd")//如果使用json-schema验证，必须添加
+        private Date birth;
+        @ExcelColumn("birth")//支持一列匹配多个属性
+        @JsonSerialize(using = LocalDateTimeSerializer.class)//如果使用json-schema验证，必须添加
+        @JsonFormat(pattern = "yyyy-MM-dd")//如果使用json-schema验证，必须添加
+        private LocalDateTime birth1;
+    }
+    @Test
+    public void test1(){
+        String filepath = System.getProperty("user.dir")+ "//testdata//test1.xlsx";
+        ImportResult importResult =  ExcelUtil.importExcel(filepath,//excle文件路径,也传excle文件的字节数组byte[],支持xls和xlsx。
+                TestModel.class,//中间类类型
+                true//是否容错处理，false则全部数据验证必须通过才执行入库操作，且入库操作只要没返回true，则不继续执行。true则只会对验证成功的记录进行入库操作，入库操作失败不影响后面的入库。
+        );
+        System.out.println(importResult);
+    }
+}
